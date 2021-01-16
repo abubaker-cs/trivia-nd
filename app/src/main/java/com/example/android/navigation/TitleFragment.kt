08d4e0e92a81f8458d090e866ca09b9915d79bf0
abubaker <mohammad_abubaker@yahoo.com>
2021-01-16 15:00:23 +0500

    Upgraded libraries and created connection between fragments using Fragment Graph.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -20,9 +21,24 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+        // Select TitleFragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false
         )
+
+        // Anonymous function
+        // ==================
+        // Navigate on clicking playButton()
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener((R.id.action_titleFragment_to_gameFragment))
+        )
+
+        // Standard Function
+        // =================
+        // binding.playButton.setOnClickListener { view: View ->
+        //    Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        // }
+
         return binding.root
 
     }
